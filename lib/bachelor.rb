cb7bb4b58bb90c3e17f0dd0a314a62fd42712e55
convivial-system-4430 <cdneal091@gmail.com>
2019-03-05 17:14:09 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -43,6 +43,8 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
+  num_contestants = 0 
+  total_age_contestants = 0
   data[season].each do  | contestant |
     if contestant['status'] == 'Winner'
       return contestant['name'].split(' ')[0]
