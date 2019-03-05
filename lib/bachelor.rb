d3a3c109b69413b8d85e7ad4bfc5b472fbf7eb4b
convivial-system-4430 <cdneal091@gmail.com>
2019-03-05 17:03:30 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -8,7 +8,11 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do | season, contestants |
+    contestants.each do | contestant |
+      binding.pry
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
