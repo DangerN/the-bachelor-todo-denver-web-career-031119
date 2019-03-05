aba0fa0e0ca19f6c5c11da9d4fb8734b00402f13
convivial-system-4430 <cdneal091@gmail.com>
2019-03-05 17:00:54 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,7 @@ require 'pry'
 def get_first_name_of_season_winner(data, season)
   data[season].each do  | contestant |
     if contestant['status'] == 'Winner'
-    binding.pry
+      return contestant['name'].split(' ')[0]
   end
 end
 
